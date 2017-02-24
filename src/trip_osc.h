553620b45a21b3435fb30cd9553b28de7bd88610
Steven Leib <leib3@uic.edu>
2017-02-24 13:17:31 -0600

    still working on getting osc to work

---
+++ src/trip_osc.h
@@ -0,0 +1,13 @@
+#include <OSCMessage.h>
+#include <SLIPEncodedUSBSerial.h>
+#include <SLIPEncodedUSBSerial.h>
+
+/*
+Make an OSC message and send it over serial
+ */
+
+
+
+void oscinit();
+void oscsend(float value);
+
